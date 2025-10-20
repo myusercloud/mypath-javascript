733a28726120b3d9d128bdb92d4747183a634848
Harrizon | Myusercloud <lucasharrizon@gmail.com>
2025-10-20 13:54:12 +0300

    refactor learning.js to update variable names and improve output formatting

--- learn/learning.js
+++ learn/learning.js
@@ -1,9 +1,9 @@
-let names = "Harry";
-let age = 19
-let driver = true;
-if (driver) {
-    console.log(names + " is a driver");
-} else {
-    console.log(names + " is not a driver");
-}
-console.log(names + " is " + age + " years old");
+let name = "Harrizon";//reassignable
+
+console.log("hello,", name);//creates a space
+console.log("hello, " + name);//concatenation, does not create a space
+
+const age = 20;//not reassignable
+console.log(name, "is", age, "years old");
+console.log(`${name} is ${age} years old`);//template literals
+console.log(name + " is " + age + " years old");//concatenation
